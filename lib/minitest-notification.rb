467c1fa3fe0709f791756c7dc0072b8ab6526eba
Keisuke KITA <kei.kita2501@gmail.com>
2013-01-02 22:08:58 +0900

    inspire of minitest/growl_notify

--- lib/minitest-notification.rb
+++ lib/minitest-notification.rb
@@ -1,6 +1,34 @@
 require "minitest-notification/version"
+require "terminal-notifier-guard"
 
 module Minitest
+  # Test notifier for minitest via notification center.
+  #
+  # == Usage
+  #
+  # In your test helper put:
+  #
+  #   require 'minitest/autorun'  
+  #   require 'minitest/notification'
+  #
   class Notification
+    def initialize(io)
+      @io = io
+    end
+
+    def puts(*o)
+      if o.first =~ /(\d+) failures, (\d+) errors/
+        description = [ defined?(RUBY_ENGINE) ? RUBY_ENGINE : "ruby", RUBY_VERSION, RUBY_PLATFORM ].join(" ")
+        if $1.to_i > 0 || $2.to_i > 0 # fail?
+          TerminalNotifier::Guard.failed(o.first, :title => ":-( #{description}")
+        else
+          TerminalNotifier::Guard.success(o.first, :title => ":-) #{description}")
+        end
+      else
+        @io.puts(*o)
+      end
+    end
   end
 end
+
+MiniTest::Unit.output = MiniTest::Notification.new(MiniTest::Unit.output)
